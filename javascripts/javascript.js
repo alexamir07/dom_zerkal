6569c7de14832f2b6badab27a0d31aba54b4d7b3
alexamir07 <alexamir753@gmail.com>
2025-02-27 19:53:08 +0500

    правки с пары

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,29 +1,33 @@
-// курсор
-document.addEventListener('mousedown', function () {
-  document.body.classList.add('clicked')
-})
-
-document.addEventListener('mouseup', function () {
-  document.body.classList.remove('clicked')
-})
-// анимация глаз
 document.addEventListener('DOMContentLoaded', () => {
   mouseMoveEyes()
+  cursor()
 })
 
+// анимация глаз
 function mouseMoveEyes() {
-  let moveArea = document.querySelector('.move-area')
+  // let moveArea = document.querySelector('.move-area')
 
-  moveArea.addEventListener('mousemove', () => {
+  document.addEventListener('mousemove', (event) => {
     let eyes = document.querySelectorAll('.eye')
 
     eyes.forEach((eye) => {
-      let x = eye.offsetLest + eye.offsetWidth / 2
+      let x = eye.offsetLeft + eye.offsetWidth / 2
       let y = eye.offsetTop + eye.offsetHeight / 2
       let rad = Math.atan2(event.pageX - x, event.pageY - y)
-      let rot = rad + (180 / Math.PI) * -1
+      let rot = rad * (180 / Math.PI) * -1
 
-      eye.style = `transform: rotate($(rot)deg);`
+      eye.style = `transform: rotate(${rot}deg);`
     })
+  })
+}
+
+// курсор
+function cursor() {
+  document.addEventListener('mousedown', function () {
+    document.body.classList.add('clicked')
+  })
+
+  document.addEventListener('mouseup', function () {
+    document.body.classList.remove('clicked')
   })
 }
