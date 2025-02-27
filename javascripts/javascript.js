4cc2a0f26ebedf72d06c2d0ca05bbd80fbb31c2b
alexamir07 <alexamir753@gmail.com>
2025-02-27 10:29:53 +0500

    нерабочая анимация глаз

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,3 +1,4 @@
+// курсор
 document.addEventListener('mousedown', function () {
   document.body.classList.add('clicked')
 })
@@ -5,3 +6,24 @@ document.addEventListener('mousedown', function () {
 document.addEventListener('mouseup', function () {
   document.body.classList.remove('clicked')
 })
+// анимация глаз
+document.addEventListener('DOMContentLoaded', () => {
+  mouseMoveEyes()
+})
+
+function mouseMoveEyes() {
+  let moveArea = document.querySelector('.move-area')
+
+  moveArea.addEventListener('mousemove', () => {
+    let eyes = document.querySelectorAll('.eye')
+
+    eyes.forEach((eye) => {
+      let x = eye.offsetLest + eye.offsetWidth / 2
+      let y = eye.offsetTop + eye.offsetHeight / 2
+      let rad = Math.atan2(event.pageX - x, event.pageY - y)
+      let rot = rad + (180 / Math.PI) * -1
+
+      eye.style = `transform: rotate($(rot)deg);`
+    })
+  })
+}
